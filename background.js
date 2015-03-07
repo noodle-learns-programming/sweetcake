3c94a89bd30b91c5cdc8f7237869a10835d8e51d
tpphu <vietean@gmail.com>
2015-03-07 22:10:53 +0700

    - Update code

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var TabManager = {};
+TabManager.arrManagedTabs = [];
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	/**
 	 |---------------------------------------------------------------------
@@ -6,9 +8,13 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	 */
     if( changeInfo.status === "complete" )
     {
+    	console.log('chrome.tabs.onUpdated | tabId: ', tabId);
+		console.log('chrome.tabs.onUpdated | changeInfo: ', changeInfo);
+		console.log('chrome.tabs.onUpdated | tab: ', tab, tab.url);
     	chrome.tabs.executeScript(tabId, { file: "jquery.min.js" }, function() {
 		    chrome.tabs.executeScript(tabId, { file: "main.js" }, function(){
 		    	console.log('Everything is ok', arguments);
+		    	TabManager.arrManagedTabs[tabId] = tab;
 		    });
 		});
 	}
@@ -18,14 +24,13 @@ chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId, changeInfo, tab) {
-	console.log('chrome.tabs.onRemoved: ', changeInfo, tab);
+	console.log('chrome.tabs.onRemoved | tabId: ', tabId);
+	console.log('chrome.tabs.onRemoved | changeInfo: ', changeInfo);
+	console.log('chrome.tabs.onRemoved | tab: ', tab, tab.url);
+	delete TabManager.arrManagedTabs[tabId];
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log(
-		sender.tab
-		? "tab:" + sender.tab
-        : "tab: undefined"
-    );
-    console.log(request);
+	console.log('chrome.runtime.onMessage | sender.tab: ', sender.tab)
+    console.log('chrome.runtime.onMessage | request: ', request)
 });
